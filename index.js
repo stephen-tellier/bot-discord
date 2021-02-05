9ed4bd741ee4fe68eaf63130dc0e4d1061da5463
stephen-tellier <57587108+stephen-tellier@users.noreply.github.com>
2021-02-05 21:21:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const request = require("request");
+
+let url = "https://api.top-serveurs.net/v1/servers/HURZR2SCR6/players-ranking";
+
+let options = { json: true };
+
+bot.on("ready", function () {});
+
+bot.on("message", function (message) {
+  if (message.content === "!topvote") {
+    request(url, options, (error, res, body) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      if (!error && res.statusCode === 200) {
+        let votelist = "";
+        let n = 1;
+        body.players.forEach((el) => {
+          let place = n;
+          if (place === 1) {
+            place = ":first_place:";
+          } else if (place === 2) {
+            place = ":second_place:";
+          } else if (place === 3) {
+            place = ":third_place:";
+          } else {
+            place = ":black_small_square:";
+          }
+
+          votelist = votelist.concat(
+            place + " **" + el.playername + "** - " + el.votes + " votes \n"
+          );
+
+          n = n + 1;
+        });
+
+        message.channel.send({
+          embed: {
+            title: "Les tops votes",
+            description: votelist,
+
+            url:
+              "https://top-serveurs.net/conan-exiles/ouvert-le-0412-les-rescapes-rppvp-vocal-conquete"
+          }
+        });
+      }
+    });
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
